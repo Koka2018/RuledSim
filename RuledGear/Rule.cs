4cbb61f78fd788d359e54b3d7df7518c0e41dd9d
Koka <a@b>
2021-12-07 07:05:00 +0900

    更新

---
+++ RuledGear/Rule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuledGear
+{
+    public class Rule
+    {
+        public string Name { get; protected set; } = string.Empty;
+
+        public Rule()
+        {
+        }
+
+        public Rule(string name)
+        {
+            if(name == null || name.Length == 0) 
+            {
+                throw new ArgumentException("name"); 
+            }
+            Name = name;
+        }
+    }
+}
---
+++ RuledGearTests/RuleTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RuledGear;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuledGear.Tests
+{
+    [TestClass()]
+    public class RuleTests
+    {
+        [TestClass]
+        public class コンストラクタ
+        {
+
+            [TestMethod()]
+            public void インスタンスできること()
+            {
+                var rule = new Rule();
+                Assert.IsNotNull(rule);
+                Assert.AreEqual(string.Empty, rule.Name);
+            }
+
+            [TestMethod()]
+            [DataRow("Name")]
+            [DataRow("@")]
+            [DataRow("$")]
+            [DataRow("a,b")]
+            [DataRow("a=b")]
+            [DataRow("a$b")]
+            [DataRow("#ab")]
+            [DataRow("あいうえお")]
+            [DataRow("12345")]
+            public void インスタンスできること_引数1(string name)
+            {
+                var rule = new Rule(name);
+                Assert.IsNotNull(rule);
+                Assert.AreEqual(name, rule.Name);
+            }
+
+
+            [TestMethod()]
+            [ExpectedException(typeof(ArgumentException))]
+            [DataRow(null)]
+            [DataRow("")]
+            public void コンストラクタの引数が不正の場合例外を発生すること(string name)
+            {
+                var rule = new Rule(name);
+            }
+        }
+    }
+}
